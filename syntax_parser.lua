08f98a3392e4aa265834b31e2bc2de817ceb3507
krh2001@gmail.com <krh2001@gmail.com@43399f44-b635-06ed-7d79-00b75291eb8c>
2011-04-07 12:29:36 +0000

    modify the syntax input format

--- syntax_parser.lua
+++ syntax_parser.lua
@@ -1,21 +1,21 @@
 local syntax_pattern = {
-	expression = {
+	{ 'expression',
 		{'add_expr', function(s, a)  return a; end },
 	},
 
-	add_expr = {
+	{ 'add_expr',
 		{'add_expr', '+', 'mul_expr', function(s, a, b, c)  return { op = b, left = a, righrt = c }; end,},
 		{'add_expr', '-', 'mul_expr', function(s, a, b, c)  return { op = b, left = a, righrt = c }; end,},
 		{'mul_expr'                   function(s, a)        return a; end },
 	},
 
-	mul_expr = {
+	{ 'mul_expr',
 		{'mul_expr', '*', 'unary_expr', function(s, a, b, c)  return { op = b, left = a, righrt = c }; end, },
 		{'mul_expr', '/', 'unary_expr', function(s, a, b, c)  return { op = b, left = a, righrt = c }; end, },
 		{'unary_expr',                  function(s, a)        return a; end },
 	},
 	
-	unary_expr = {
+	{ 'unary_expr',
 		{'-', 'expression',,          function(s, a, b)        return { op = 'neg', right = b, } end, },
 		{'(', 'expression', ')',      function(s, a, b, c)     return c; end, },
 		{'sym', '(', 'arglist', ')',  function(s, a, b, c, d)  return { op ='call', fun = a, args = c}; end, },
@@ -24,23 +24,23 @@ local syntax_pattern = {
 	},
 	
 	
-	arglist = {
+	{ 'arglist',
 		{'expression', function(s, a)  return { a }; end, }
 		{'arglist', 'expression', function(s, a, b)  a[table.getn(a)+1] = b;  return { a }; end, }
 	},
+	
+	start = 'expression',
 	
 	trace = print,
 	
 };
 
 
-
-local syntax, err = syntax_create(syntax_pattern, 'expression');
+local syntax, err = syntax_create(syntax_pattern);
 
 if(not syntax) then
 	error(err);
 end
-
 
 local tree = syntax.parse(l);
 
