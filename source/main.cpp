cb3b423c1bd6d0146f8c73996be32a4fc75ba1d7
krh2001 <krh2001@gmail.com>
2012-10-11 14:35:54 +0000

    new vc6 project and main source files

---
+++ source/main.cpp
@@ -0,0 +1,37 @@
+#define  WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include "../include/resource.h"
+
+
+static BOOL CALLBACK MainDialogCallback(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int CALLBACK WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd )
+{
+	DialogBox(hInstance, MAKEINTRESOURCE(IDD_MAIN), NULL, MainDialogCallback);
+
+	return 0;
+
+}
+
+
+static BOOL CALLBACK MainDialogCallback(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch(uMsg)
+	{
+
+	case WM_COMMAND:
+		if(LOWORD(wParam) == IDOK)
+		{		
+			EndDialog(hDlg, IDOK);
+			return TRUE;
+		}
+		else if(LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, IDCANCEL);
+			return TRUE;
+		}
+	
+		break;
+	}
+	return FALSE;
+}
