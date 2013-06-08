482896883cb3dfea80c3415b1589c345756361a8
krh2001 <krh2001@43399f44-b635-06ed-7d79-00b75291eb8c>
2013-06-08 03:47:20 +0000

    add logic test project

---
+++ test/logic/src/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define PROJ_NAME    "Test Logic"
+#define VERSION_STR  "v0.1.0"
+
+
+int main(int argc, char** argv)
+{
+	printf(PROJ_NAME" "VERSION_STR"\n");
+	return 0;
+}
