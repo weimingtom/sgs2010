6fa0a43b16fd9e553baa4ace09007e19275f0607
krh2001@gmail.com <krh2001@gmail.com@43399f44-b635-06ed-7d79-00b75291eb8c>
2013-09-01 16:09:01 +0000

    fix embed code for multi-line comment error and other bugs

--- trunk/3p/tolua++-1.0.93/src/bin/lua/clean.lua
+++ trunk/3p/tolua++-1.0.93/src/bin/lua/clean.lua
@@ -45,7 +45,14 @@ function clean (s)
 
  local S = "" -- saved string
 
+--[[ 
+-- 这个处理有问题。
+ -- 1）不能处理多行注释，
+ -- 2）不支持 %[(=*)%[.-%]%1%] 格式的字符串，
+ -- 3）字符串中的转义不支持 "\"" 这种 
+ -- 4）可能会改变串的内容（空白）
  s = mask(s)
+
 
  -- remove blanks and comments
  while 1 do
@@ -74,6 +81,96 @@ function clean (s)
  S = gsub(S,"[ \t]*\n[ \t]*","\n")
 	S = gsub(S,"\n+","\n")
  S = unmask(S)
- return S
+ --]]
+ -- 使用我的算法代替它
+ local stb = {n=1, [1]='$'}; -- 保存串表 将所有的字符串替换为 $1;,$2;,...,$n;
+ 
+ --print('>>>>>>>>>>>>> before clean\n'..s);
+
+ local xfind = function(text, treg)
+   local s,e, s1,e1;
+   for _, r in ipairs(treg) do
+     s1,e1 = strfind(text, r);
+	 if s1 then
+	   if not s or s1 < s or (s1 == s and  e1 > e) then
+	     s,e = s1,e1
+	   end
+	 end
+   end
+   return s,e;
+  end
+  
+  while  s ~= '' do
+	local b, e = xfind(s, {'%$', '\'', '\"', '%[=*%[', '%-%-%[%[', '%-%-'});
+	if b then
+	  S = S .. strsub(s, 1, b-1);
+	  local sy = strsub(s, b,e);
+	  s = strsub(s, e+1);
+	  if sy == '\'' or sy == '\"' then
+		local sss = sy;
+	    while 1 do
+		  local sb,se = xfind(s, {sy, '\\'..sy})
+		  local ssy = strsub(s, sb,se);
+		  sss = sss .. strsub(s, 1, se+0);
+		  s = strsub(s, se+1);
+		  if strsub(ssy,1,1) ~= '\\' then
+		    break;
+		  end
+		end 
+		if stb[sss] ~= nil then
+		  S = S .. '$' .. tostring(stb[sss]) .. ';';
+		else
+		  stb.n = stb.n + 1
+		  stb[stb.n] = sss;
+		  stb[sss] = stb.n;
+		  S = S .. '$' .. tostring(stb.n) .. ';';
+		end
+	  elseif strfind(sy, '^%[=*%[$') then
+		local sss = sy;
+		local ey = gsub(sy, '%[', ']');
+		local sb,se = xfind(s, {ey});
+		sss = sss .. strsub(s, 1, se);
+		s = strsub(s, se+1);
+		if stb[sss] ~= nil then
+		  S = S .. '$' .. tostring(stb[sss]) .. ';';
+		else
+		  stb.n = stb.n + 1
+		  stb[stb.n] = sss;
+		  stb[sss] = stb.n;
+		  S = S .. '$' .. tostring(stb.n) .. ';';
+		end
+	  elseif sy == '$' then
+	    S = S .. '$1;' 
+	  elseif sy == '--[[' then
+		local sb,se = xfind(s, {'%-%-%]%]'});
+		s = strsub(s, se+1);
+	  elseif sy == '--' then
+		local sb,se = xfind(s, {'\n'});
+		if sb then
+		  S = S .. '\n';
+		  s = strsub(s, se+1);
+		else
+		  s = '';
+		end
+	  end
+	else
+	  S = S .. s;
+	  s = '';
+	end
+  end	
+  --print('>>>>>>>>>>>>> string table');
+  --for i, v in ipairs(stb) do print(i,' = ', v) end 
+  --print('>>>>>>>>>>>>> after prep\n'..S);
+ 
+ -- eliminate unecessary spaces
+ S = gsub(S,"[ \t]+"," ")
+ S = gsub(S,"[ \t]*\n[ \t]*","\n")
+ S = gsub(S,"\n+","\n")
+ 	 
+ --print('>>>>>>>>>>>>> after reduce space\n'..S);
+ -- restore strings
+  S = gsub(S, "%$(%d+);", function (a) return stb[tonumber(a)]; end);	
+  --print('>>>>>>>>>>>>> after unmask\n'..S);
+  return S
 end
 
