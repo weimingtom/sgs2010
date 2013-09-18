b69f26e5eadf255487fb7827478e6f68632d1106
krh2001@gmail.com <krh2001@gmail.com@43399f44-b635-06ed-7d79-00b75291eb8c>
2013-09-18 15:36:47 +0000

    add dianwei script

--- trunk/test/logic/script/hero/dianwei.lua
+++ trunk/test/logic/script/hero/dianwei.lua
@@ -15,3 +15,88 @@ WEI012　小冷
 
 --]]
 
+
+local cfg = {
+	sid = "dianwei",
+	name = "典韦",
+	desc = [==[【古之恶来·典韦】
+强袭——出牌阶段，你可以自减1点体力或弃一张武器牌，然后对你攻击范围内的一名角色造成1点伤害。每回合限用一次。
+★若同时用到当前装备的武器攻击范围时，不能弃掉这张武器牌。
+★若仅剩1点体力时发动此技能，且进入濒死状态无人救回，则典韦死亡后，目标角色受到1点伤害。]==],
+	group = HeroGroup_Wei,
+	sex = HeroSex_Male,
+	master = NO,
+	life = 4,
+};
+
+
+local qiangxi = {
+	name = "强袭",
+	flag=0,
+	can_use = { },
+	event = { },
+};
+
+qiangxi.can_use[GameEvent_RoundOutCard] = function(cfg, game, event, player)
+	if event.trigger == player
+		and not string.find(event.ud, '[qiangxi]', 1, true)
+	then
+		return USE_MANUAL;
+	end
+	return USE_CANNOT;
+end
+
+
+qiangxi.event[GameEvent_RoundOutCard] = function(cfg, game, event, player)
+	-- 选择一个目标, 按杀的攻击范围检测
+	local target = select_target_check(game, event, player, get_card_id_by_sid('sha'), NO, NO, 
+		'请选择一个攻击范围内的目标角色：',
+		function (t) return true; end);
+
+	-- 选择自减一点体力还是弃武器牌
+	local sel = game_select_items(game, event, player, '自减[1]点体力\n弃置武器牌\n(c)取消\n', '请选择：');
+	if sel == 1 or sel == 2 then
+		if sel == 1 then
+			if R_SUCC ~= game_player_add_life(game, event, player, -1, player, nil, 1) then
+				return R_DEF;
+			end
+			-- 这里可能自己已死，不过，不过技能应该继续走完的。
+		else
+			if get_player_equipcard(get_game_player(game, player), EquipIdx_Weapon) == nil then
+				message('你没有装备武器, 发动技能失败！');
+				return R_DEF;
+			end
+			-- 再次检查攻击范围（因为可能弃了武器之后攻击范围已改变）
+			set_player_card_flag(get_game_player(game, player), CardWhere_PlayerEquip, EquipIdx_Weapon, CardFlag_InUse);
+			local r = game_check_attack(game, event, player, target, get_card_id_by_sid('sha'), 0))
+			set_player_card_flag(get_game_player(game, player), CardWhere_PlayerEquip, EquipIdx_Weapon, CardFlag_None);
+			if r ~= R_SUCC then
+				message('你不能弃置该武器！');
+				return R_DEF;
+			end
+			if R_SUCC ~= game_player_discard_card(game, event, player, CardWhere_PlayerEquip, EquipIdx_Weapon) then
+				return R_DEF;
+			end
+		end
+		-- 效果目标受到伤害
+		
+		-- 只能使用一次
+	else
+		-- canceled;
+		return R_DEF;
+	end
+end
+
+
+cfg.skills = {
+	qiangxi,
+};
+
+
+
+reg_hero(cfg);
+
+
+
+
+
