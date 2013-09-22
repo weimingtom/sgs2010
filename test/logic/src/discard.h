691f996d42d8414cdc6b51a4da61736210d539f5
krh2001 <krh2001@gmail.com>
2013-09-22 15:58:12 +0000

    fix linux compile errors

--- test/logic/src/discard.h
+++ test/logic/src/discard.h
@@ -57,7 +57,7 @@ RESULT game_cmd_discard_card(GameContext* pGame, GameEventContext* pParentEvent,
 RESULT game_player_discard_card(tolua_notnull GameContext* pGame, tolua_notnull GameEventContext* pParentEvent, int player, CardWhere where, int pos);
 
 RESULT game_passive_discard(lua_State* L, tolua_notnull GameContext* pGame, tolua_notnull GameEventContext* pParentEvent
-							, int player, int where, int num, YESNO force, const char* alter_text, int* real_num = NULL);
+							, int player, int where, int num, YESNO force, const char* alter_text, int* real_num = 0);
 YESNO is_cur_card_valid(GameContext* pGame, CardWhere where, int pos);
 RESULT add_cur_card_to_player_hand(lua_State* L, tolua_notnull GameContext* pGame, CardWhere where, int pos, int player);
 RESULT add_cur_card_to_player_judgment(lua_State* L, tolua_notnull GameContext* pGame, tolua_notnull Card* v, tolua_notnull PosCardList* list, int player);
